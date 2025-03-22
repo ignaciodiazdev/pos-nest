a7ae10b9156124dd64114a4a7c547adc00e561bf
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-22 17:57:07 -0500

    add: Agregar atributos y relacion ManyToOne con Category

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Category } from "src/categories/entities/category.entity";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'varchar', length: 60})
+    name: string;
+
+    @Column({type: 'varchar', length: 120, nullable: true, default: 'default.svg'})
+    image: string;
+
+    @Column({type: 'decimal'})
+    price: number
+
+    @Column({type: 'int'})
+    inventory: number;
+
+    @ManyToOne(() => Category)
+    category: Category
+}
