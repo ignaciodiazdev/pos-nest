65acb190c46c671bed1d05e7263435d9eafeae1f
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-22 11:55:17 -0500

    add: Configurar TypeORM y conectar a la DB Postgres de Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService]
+    }),
+    CategoriesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
