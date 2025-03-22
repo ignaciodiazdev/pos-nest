29d86bca9f9e5221a3dd44fcab1f2b863b7cb16f
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-22 18:31:45 -0500

    add: Agregar propiedad y validacion a CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsPositive, IsString, Min } from "class-validator"
+
+export class CreateProductDto {
+
+    @IsNotEmpty({message: 'El Nombre del Producto es Obligatorio'})
+    @IsString({message: 'Nombre no válido'})
+    name: string
+
+    
+    @IsNotEmpty({message: 'El Precio del Producto es Obligatorio'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'Precio no válido'})
+    price: number
+
+    @IsNotEmpty({message: 'La cantidad no puede ir vacia'})
+    @IsNumber({maxDecimalPlaces: 0}, {message: 'Cantidad no válida'})
+    inventory: number
+
+    @IsNotEmpty({message: 'La categoría es Obligatoria'})
+    @IsInt({message: 'La cateogoría no es válida'})
+    categoryId: number
+}
