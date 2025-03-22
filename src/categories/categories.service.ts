852e85d46a7d2cb5fbc99febfcdf8792647fcd9b
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-22 16:47:44 -0500

    add: Crear y obtener categorias

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -14,15 +14,18 @@ export class CategoriesService {
   ){}
 
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    return this.categoryRepository.save(createCategoryDto);
   }
 
   findAll() {
-    return `This action returns all categories`;
+    return this.categoryRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOneBy({id})
+    if(!category)
+      throw new NotFoundException('La Categoría no existe')
+    return category;
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
